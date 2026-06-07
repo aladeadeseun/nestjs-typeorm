ad8bdf88091567536a0333f81f2aa2c072c12666
Alade Adeseun <aladeadeseun@gmail.com>
2026-06-07 17:17:53 +0100

    new update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, ItemsModule],
   controllers: [AppController],
   providers: [AppService],
 })
